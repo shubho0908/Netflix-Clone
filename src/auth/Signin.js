633bd6d5811a0f9b0c73eab74ac6e0e0c41bddad
Shubhojeet Bera <shubhobera98@gmail.com>
2023-02-15 21:13:40 +0530

    project updaed

--- src/auth/Signin.js
+++ src/auth/Signin.js
@@ -3,22 +3,40 @@ import "./Auth.css";
 // IMG Files
 import Logo from "../img/logo.png";
 
-
 const Signin = () => {
   return (
     <>
-    <div className="navbar">
+      <div className="navbar">
         <div className="logo">
           <img className="Netflix-logo" src={Logo} alt="" />
         </div>
+      </div>
+      <div className="main-page">
         <div className="container">
-            <div className="signup-page">
-                
+          <div className="signin-page">
+            <h2 className="signin-head">Sign In</h2>
+            <div className="input-fields">
+              <div className="email">
+                <input
+                  className="email-inp"
+                  type="text"
+                  placeholder="Email or phone number"
+                />
+              </div>
+              <div className="password">
+                <input
+                  className="password-inp"
+                  type="password"
+                  placeholder="Password"
+                />
+              </div>
             </div>
+            <button className="signin-button">Sign In</button>
+          </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Signin
+export default Signin;
