695346f5437d565c17f2c3e0ee8a70d4377db9ab
Shubhojeet Bera <shubhobera98@gmail.com>
2023-02-19 17:52:38 +0530

    project updated

--- src/Component/Browsepage.js
+++ src/Component/Browsepage.js
@@ -1,65 +1,105 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
 import { getAuth, signOut } from "firebase/auth";
 import { app } from "../Firebase";
 import Poster from "../img/movie/bg-poster.png";
 import avatar from "../img/avatar.png";
 import Logo from "../img/logo.png";
+import Search from '../img/search.png'
+import left from '../img/left.png'
+import right from '../img/right.png'
 
 const auth = getAuth(app);
 
 const Browsepage = () => {
+  const [movies, setMovies] = useState([]);
 
-const changeBG = ()=>{
-  const navbar = document.querySelector('.navbar-4')
-  const scrollVal = window.scrollY
-  // console.log(scrollVal);
-  if (scrollVal < 42) {
-    navbar.classList.remove('nav-css')
-  }
-  else{
-    navbar.classList.add('nav-css')
+  const changeBG = () => {
+    const navbar = document.querySelector(".navbar-4");
+    const scrollVal = window.scrollY;
+    // console.log(scrollVal);
+    if (scrollVal < 42) {
+      navbar.classList.remove("nav-css");
+    } else {
+      navbar.classList.add("nav-css");
+    }
+  };
 
-  }
-}
+  window.addEventListener("scroll", changeBG);
 
-window.addEventListener("scroll", changeBG)
+  useEffect(() => {
+    const FetchData = async () => {
+      const url =
+        "https://api.themoviedb.org/3/movie/popular?api_key=0bee82696d9f2ec6851e2a729cf4c379&language=en-US&page=1";
+      const data = await fetch(url);
+      const new_data = await data.json();
+      setMovies(new_data.results);
+      console.log(movies);
+    };
+
+    FetchData();
+  }, []);
 
   return (
     <>
       <div className="App">
-        <div className="browse-page">
-          <div className="navbar-4 nav-css">
-            <img src={Logo} className="main-logo" />
-            <div className="category-lists">
-              <li>Home</li>
-              <li>TV Shows</li>
-              <li>Trending</li>
-              <li></li>
+        <div className="navbar-4 nav-css">
+          <img src={Logo} className="main-logo" />
+          <div className="category-lists">
+            <li style={{ fontWeight: 600 }}>Home</li>
+            <li style={{ width: "4.5vw" }}>TV Shows</li>
+            <li style={{ width: "3vw" }}>My List</li>
+            <li>Popular</li>
+            <li></li>
+          </div>
+          <div className="right-content">
+            <div className="search-icon">
+              <img src={Search} alt="" className="search-white" />
             </div>
-            <div className="right-content">
-              <div className="search-icon">
-                <svg className="search-img"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  fill="currentColor"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-                </svg>
-              </div>
-              <div className="account">
-                <img className="account-img" src={avatar} alt="" />
-              </div>
+            <div className="account">
+              <img className="account-img" src={avatar} alt="" />
             </div>
           </div>
-          <div className="bg-main-data">
-            <img src={Poster} alt="" className="poster" />
-            <button className="play1">&#9654; Play</button>
+        </div>
+        <div className="bg-main-data">
+          <img src={Poster} alt="" className="poster" />
+          <button className="play1">&#9654; Play</button>
+        </div>
+        <div className="movies-section">
+            <div className="left-scroll">
+              <img onClick={()=>{
+                const carousel = document.querySelector('.movie-category1')
+                carousel.scrollLeft -= 1200
+
+              }} src={left} alt="" className="left-btn" />
+            </div>
+            <div  className="right-scroll">
+            <img onClick={()=>{
+                const carousel = document.querySelector('.movie-category1')
+                carousel.scrollLeft += 1200
+
+              }} src={right} alt="" className="right-btn" />
+            </div>
+          <div className="movie-category1">
+            {movies.map((items) => {
+              return (
+                <>
+                  <div className="newboy">
+                    <img
+                      src={
+                        "https://image.tmdb.org/t/p/w500" + items.backdrop_path
+                      }
+                      alt=""
+                      className="poster-2"
+                    />
+                  </div>
+                </>
+              );
+            })}
+            
           </div>
-          <div className="movies-section">
-            <div className="movie-category1"></div>
-          </div>
+          <div className="movie-category2"></div>
+          <div className="movie-category3"></div>
+          <div className="movie-category4"></div>
         </div>
       </div>
     </>
